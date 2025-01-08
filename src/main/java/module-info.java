c35da5069ddcaf604c58a0985225c31cc150dcbd
EternalProg <eternal.proglife@gmail.com>
2025-01-08 11:34:10 +0200

    Changed module-info.java file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,9 @@
 module com.dmitrii.audioplayer {
     requires javafx.controls;
     requires javafx.fxml;
-    requires spring.boot;
-    requires spring.boot.autoconfigure;
-    requires spring.context;
-    requires spring.beans;
-    requires spring.core;
-    requires jakarta.persistence;
 
-    opens com.dmitrii.audioplayer to spring.core, spring.context;
+    opens com.dmitrii.audioplayer;
     opens com.dmitrii.audioplayer.controller to javafx.fxml;
-    opens com.dmitrii.audioplayer.model to spring.core, spring.context;
 
     exports com.dmitrii.audioplayer;
     exports com.dmitrii.audioplayer.controller;
