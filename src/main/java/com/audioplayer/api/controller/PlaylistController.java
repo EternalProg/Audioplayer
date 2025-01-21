127104e7aff43d5bb1c374e0794fb2c277991c59
EternalProg <eternal.proglife@gmail.com>
2025-01-21 18:54:35 +0200

    Added plylist api logic

--- src/main/java/com/audioplayer/api/controller/PlaylistController.java
+++ src/main/java/com/audioplayer/api/controller/PlaylistController.java
@@ -1,4 +1,67 @@
 package com.audioplayer.api.controller;
 
+import com.audioplayer.api.DTO.PlaylistCreateDTO;
+import com.audioplayer.api.DTO.PlaylistDTO;
+import com.audioplayer.api.service.PlaylistService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/playlists")
 public class PlaylistController {
+    private final PlaylistService playlistService;
+
+    public PlaylistController(PlaylistService playlistService) {
+        this.playlistService = playlistService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PlaylistDTO>> getAllPlaylists() {
+        return ResponseEntity.ok(playlistService.getAllPlaylists());
+    }
+
+    @GetMapping("/by-userid/{userId}")
+    public ResponseEntity<Page<PlaylistDTO>> getUserPlaylists(@PathVariable Long userId, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(playlistService.getUserPlaylists(userId, page, size));
+    }
+
+    @GetMapping("/{playlistId}")
+    public ResponseEntity<PlaylistDTO> getPlaylist(@PathVariable Long playlistId) {
+        Optional<PlaylistDTO> playlist = playlistService.getPlaylistById(playlistId);
+        return playlist.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/by-title/{userId}/{title}")
+    public ResponseEntity<PlaylistDTO> getPlaylist(@PathVariable String title, @PathVariable Long userId) {
+        Optional<PlaylistDTO> playlist = playlistService.getPlaylist(title, userId);
+        return playlist.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<PlaylistDTO> createPlaylistForUser(@PathVariable Long userId, @RequestBody PlaylistCreateDTO playlistCreateDTO) {
+        Optional<PlaylistDTO> playlistDTOOpt = playlistService.savePlaylistForUser(userId, playlistCreateDTO);
+        return playlistDTOOpt.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{playlistId}")
+    public ResponseEntity<PlaylistDTO> updatePlaylist(@PathVariable Long playlistId, @RequestBody PlaylistCreateDTO playlistCreateDTO) {
+        Optional<PlaylistDTO> playlistDTO = playlistService.updatePlaylist(playlistId, playlistCreateDTO);
+        return playlistDTO.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{playlistId}")
+    public ResponseEntity<String> deletePlaylist(@PathVariable Long playlistId) {
+        if (playlistService.deletePlaylist(playlistId)) {
+            return ResponseEntity.ok("Playlist deleted");
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/com/audioplayer/api/repository/PlaylistRepository.java
+++ src/main/java/com/audioplayer/api/repository/PlaylistRepository.java
@@ -4,13 +4,19 @@ import com.audioplayer.api.model.Playlist;
 import com.audioplayer.api.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface PlaylistRepository extends PagingAndSortingRepository<Playlist, Long> {
-    Page<Playlist> findByUser(User user, Pageable pageable);
+@Repository
+public interface PlaylistRepository extends PagingAndSortingRepository<Playlist, Long>, JpaRepository<Playlist, Long> {
+    Page<Playlist> findPlaylistsByUserId(Long UserId, Pageable pageable);
 
-    Page<Playlist> findByName(String name, Pageable pageable);
+    Optional<Playlist> findPlaylistByTitleAndUserId(String title, Long userId);
+
+    Optional<Playlist> findPlaylistById(Long playlistId);
+
+
 }
